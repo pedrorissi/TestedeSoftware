9e24134f2afa471741b606a5fa557d72c39c37cb
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-09-16 20:00:20 -0300

    Divisão das partes que comporao o caso de teste

--- test/sistemabancario/GerenciadoraClienteTest.java
+++ test/sistemabancario/GerenciadoraClienteTest.java
@@ -14,6 +14,7 @@ public class GerenciadoraClienteTest {
 	public void testpesquisaCliente(){
 		
 		/*Criando clientes*/
+		//Definição e inicialização do cenário.
 		Cliente cliente01 = new Cliente(1, "Guilherme", 30, "guilherme@gmail.com", 1, true);
 		Cliente cliente02 = new Cliente(2, "Gabriel", 23, "gabriel@gmail.com", 2, true);
 		
@@ -22,10 +23,23 @@ public class GerenciadoraClienteTest {
 		clientes.add(cliente02);
 		
 		GerenciadoraClientes gerClientes = new GerenciadoraClientes(clientes);
+		//Fim do cenário
+		
+		//Execução
 		Cliente cliente = gerClientes.pesquisaCliente(1);
+		
+		//Análise do resultado esperado
 		assertThat(cliente.getId(), is(1));
 		assertThat(cliente.getEmail(), is("guilherme@gmail.com"));
 		
+		Cliente cliente2 = gerClientes.pesquisaCliente(2);
+		assertThat(cliente2.getId(), is(2));
+		assertThat(cliente2.getEmail(), is("gabriel@gmail.com"));
+		
 		
 	}
-}
+	
+	
+} 
+
+
